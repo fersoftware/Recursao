568c834888fde376375e459f6925de920822580b
FERNANDO ALVES DA SILVA <fersoftware@gmail.com>
2016-06-04 20:33:24 -0300

    add

--- Recursao/Program.cs
+++ Recursao/Program.cs
@@ -10,6 +10,95 @@ namespace Recursao
     {
         static void Main(string[] args)
         {
+            int[] n = { 1, 6, 10, 4, 5};
+
+            ExibeNate0REcursivo(8);
+            Console.WriteLine();
+            ExibeNate0(8);
+            Console.WriteLine();
+            Console.WriteLine("valor final =" + ExibeFatorialRecursivo(4));
+            Console.WriteLine();
+            Console.WriteLine("valor final =" + ExibeSomaRecursivo(5));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("valor final =" +  ExibeVetorRecursivo(n,n.Length-1));
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static void ExibeNate0(int n)
+        {
+            for (int i = n; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }            
+        }
+
+        static void ExibeNate0REcursivo(int n)
+        {
+            if (n < 0)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                ExibeNate0REcursivo(n - 1);
+            }
+        }
+
+        static int ExibeFatorialRecursivo(int n)
+        {
+            Console.WriteLine("n =" + n);
+
+            if (n == 0)
+            {
+                Console.WriteLine();
+                int fat = 1;                
+                return fat;
+            }
+            else
+            {
+                int fat = n * ExibeFatorialRecursivo(n - 1);
+                Console.WriteLine("fat =" + fat);
+                return fat; // Fatorial de n é n * (n-1)!
+            }
+        }
+
+        static int ExibeSomaRecursivo(int n)
+        {
+            Console.WriteLine("n =" + n);
+
+            if (n == 0)
+            {
+                Console.WriteLine();
+                int soma = 0;               
+                return soma;
+            }
+            else
+            {
+                int soma = n + ExibeSomaRecursivo(n - 1);
+                Console.WriteLine("soma =" + soma);
+                return soma; 
+            }
+        }
+      
+        static int ExibeVetorRecursivo(int[] vetor, int ind)
+        {            
+            Console.WriteLine("valor =" + vetor[ind]);
+
+            if (ind == 0)
+            {
+                Console.WriteLine();
+                int soma = 1;                
+                return soma;
+            }
+            else
+            {
+                int soma = vetor[ind] + ExibeVetorRecursivo(vetor, ind - 1);
+                Console.WriteLine("soma =" + soma);
+                return soma;
+            }
         }
     }
 }
